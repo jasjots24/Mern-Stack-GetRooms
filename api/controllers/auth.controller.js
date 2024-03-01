adaeb5c1de16934d75929ef9d237d89e166b02a0
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2024-03-01 16:27:03 +0530

    add redux persist

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,7 +22,7 @@ export const signin = async (req , res , next)=>{
         const validUser = await User.findOne({email});
         if(!validUser) return next(errorHandler(404,'User not found!'));
         const validPassword = bcryptjs.compareSync(password ,validUser.password);
-        if(!validPassword) return next(errorHandler(401 ,"Invalid Password"));
+        if(!validPassword) return next(errorHandler(401 ,"Wrong Credentials!"));
         const token = jwt.sign({id: validUser._id},process.env.JWT_SECERT);
         const {password:pass, ...rest} = validUser._doc;
         res.cookie('access_token', token ,{httponly:true})
