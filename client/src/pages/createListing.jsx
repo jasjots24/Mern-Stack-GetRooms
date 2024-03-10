45ea8ec83cb7474900ca3c1c1a7a5fe36fc01133
avikuuuu <ku.kumar.avdesh786@gmail.com>
2024-03-10 22:57:38 +0530

    fixed

--- client/src/pages/createListing.jsx
+++ client/src/pages/createListing.jsx
@@ -5,7 +5,7 @@ import {
   ref,
   uploadBytesResumable,
 } from 'firebase/storage';
-import { app } from '../firebase';
+import {app} from '../../firebase.js'
 import { set } from 'mongoose';
 
 export default function CreateListing() {
