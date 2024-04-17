19bd2c16b35656f201393c7a298aecd072dce470
Jyotpreet Kaur <justechere@gmail.com>
2024-04-17 15:53:10 +0530

    bug fix

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -50,16 +50,17 @@ export const updateListing = async (req, res, next) => {
   }
 };
 
-export const getListing = async (req, res, next) => {};
-try {
-  const listing = await Listing.findById(req.params.id);
-  if (!listing) {
-    return next(errorHandler(404, "Listing not found!"));
+export const getListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
   }
-  res.status(200).json(listing);
-} catch (error) {
-  next(error);
-}
+};
 
 export const getListings = async (req, res, next) => {
   try {
